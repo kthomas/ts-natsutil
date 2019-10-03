a79fa6f37b362405724fe92bd37df64caceb229d
Kyle Thomas <kyle@provide.services>
2019-10-03 12:08:24 -0400

    Cleanup nats wrapper and environment config parsing

--- src/env.ts
+++ src/env.ts
@@ -1,4 +1,4 @@
-import * as fs from 'fs'
+// import * as fs from 'fs'
 import { TlsOptions } from 'tls'
 
 export class Config {
@@ -17,22 +17,21 @@ export class Config {
 
   static fromEnv(overrides?: Partial<Config>): Config {
     const instance = new Config();
-    (process.env as any).forEach((k: string) => {
-      const val: any = process.env[k]
-      if (val !== undefined) {
-        (instance as any)[Config.camelize(k, '_')] = val
+    for (const [k, v] of Object.entries(process.env)) {
+      if (v !== undefined) {
+        (instance as any)[Config.camelize(k, '_')] = v
       }
-    })
+    }
 
     for (let key in (overrides || {})) {
-      instance[key] = overrides[key]
+      (instance as any)[key] = (overrides as any)[key]
     }
 
     return instance
   }
 
-  public natsClientPrefix: string | 'ts-natsutil' = process.env.NATS_CLIENT_PREFIX
-  public natsClusterId: string = process.env.NATS_CLUSTER_ID
+  public natsClientPrefix: string = process.env.NATS_CLIENT_PREFIX || 'ts-natsutil'
+  public natsClusterId?: string = process.env.NATS_CLUSTER_ID
   public natsDeadLetterSubject: string = process.env.NATS_DEADLETTER_SUBJECT || 'nats.deadletter'
   public natsEncoding: BufferEncoding = (process.env.NATS_BUFFER_ENCODING || 'utf-8') as BufferEncoding
   public natsJson: boolean | true = process.env.NATS_JSON === 'true'
@@ -40,15 +39,15 @@ export class Config {
   public natsNoEcho: boolean | false = process.env.NATS_NO_ECHO === 'true'
   public natsPedantic: boolean | false = process.env.NATS_PEDANTIC === 'true'
   public natsPingInterval: number = process.env.NATS_PING_INTERVAL ? parseInt(process.env.NATS_PING_INTERVAL) : 120000
-  public natsServers: string = process.env.NATS_SERVERS
+  public natsServers?: string = process.env.NATS_SERVERS
   public natsTlsConfigured: boolean = !!process.env.NATS_TLS_KEY && !!process.env.NATS_TLS_CERTIFICATE && !!process.env.NATS_CA_CERTIFICATE
-  public natsTlsOptions: TlsOptions = this.natsTlsConfigured ? {
+  public natsTlsOptions?: TlsOptions = this.natsTlsConfigured ? {
     // key: fs.readFileSync(process.env.NATS_TLS_KEY),
     // cert: fs.readFileSync(process.env.NATS_TLS_CERTIFICATE),
     // ca: [ fs.readFileSync(process.env.NATS_CA_CERTIFICATE) ],
     rejectUnauthorized: !(process.env.NATS_FORCE_TLS === 'true'),
-  } as TlsOptions : null
-  public natsToken: string = process.env.NATS_TOKEN
+  } as TlsOptions : undefined
+  public natsToken?: string = process.env.NATS_TOKEN
   public natsVerbose: boolean | false = process.env.NATS_VERBOSE === 'true'
 }
 
--- src/nats.ts
+++ src/nats.ts
@@ -1,12 +1,12 @@
-import * as nats from 'nats'
+import * as nats from 'ts-nats'
 import * as stan from 'node-nats-streaming'
 // import { Client as NATSClient } from 'nats'
 // import { Stan as NATSStreamingClient } from 'node-nats-streaming'
 
 import { default as Config } from './env'
 
-export type NATSClient = nats.Client
-export type NATSStreamingClient = stan.Stan
+// export type NATSClient = nats.Client
+// export type NATSStreamingClient = stan.Stan
 export type NATSMessage = stan.Message
 
 const uuidv4 = require('uuid/v4')
@@ -16,16 +16,18 @@ class NatsUtil {
   private config: Config
   private clusterId: string
   private servers: string[]
-  private token: string
+  private token?: string
+  private bearerToken?: string
 
-  constructor(clusterId?: string, servers?: string[], token?: string) {
+  constructor(clusterId: string, servers?: string[], bearerToken?: string, token?: string) {
+    this.bearerToken = bearerToken
     this.config = Config.fromEnv()
     this.clusterId = clusterId ? clusterId : this.config.natsClusterId
     this.servers = servers ? servers : this.config.natsServers.split(',')
     this.token = token ? token : this.config.natsToken
   }
 
-  getNatsConnectionOpts(clientId: string): nats.ClientOpts {
+  getNatsConnectionOpts(clientId: string): nats.NatsConnectionOptions {
     return {
       encoding: this.config.natsEncoding,
       json: this.config.natsJson,
@@ -39,44 +41,47 @@ class NatsUtil {
       servers: this.servers,
       token: this.token, 
       tls: this.config.natsTlsOptions,
+      userJWT: this.bearerToken,
       pedantic: this.config.natsPedantic,
       verbose: this.config.natsVerbose,
-    } as nats.ClientOpts
+    } as nats.NatsConnectionOptions
   }
 
-  async getNatsConnection(): Promise<NATSClient> {
-    var client: NATSClient
+  async getNatsConnection(): Promise<nats.Client> {
+    let client: nats.Client
     const clientId = `${this.config.natsClientPrefix}-${uuidv4()}`
     try {
       var opts = this.getNatsConnectionOpts(clientId) as stan.StanOptions
       // TODO: merge StanOptions into opts
-      client = await nats.connect(opts) as NATSClient;
+      client = await nats.connect(opts) as nats.Client
       client.on('connect', function() {
 
         client.on('close', function(err: any) {
-          console.log(`NATS connection ${clientId} closed; ${err}"`);
+          console.log(`NATS connection ${clientId} closed; ${err}"`)
         })
 
         client.on('error', function(err: any) {
-          console.log(`Error on NATS connection: ${clientId}; ${err}"`);
+          console.log(`Error on NATS connection: ${clientId}; ${err}"`)
         })
       })
+      return client
     } catch (err) {
-      console.log(`Error establishing NATS connection: ${clientId}; ${err}"`);
+      console.log(`Error establishing NATS connection: ${clientId}; ${err}"`)
     }
-    return client
+    throw 'failed to get NATS connection'
   }
 
-  async getNatsStreamingConnection(): Promise<NATSStreamingClient> {
-    var client: NATSStreamingClient
+  async getNatsStreamingConnection(): Promise<stan.Stan> {
+    var client: stan.Stan | undefined
     const clientId = `${this.config.natsClientPrefix}-${uuidv4()}-${this.clusterId}-${uuidv4()}`
     try {
       const opts = this.getNatsConnectionOpts(clientId)
-      client = await stan.connect(this.clusterId, clientId, opts) as NATSStreamingClient;
+      client = await stan.connect(this.clusterId, clientId, opts)
+      return client
     } catch (err) {
-      console.log(`Error establishing NATS streaming connection: ${clientId}; ${err}"`);
+      console.log(`Error establishing NATS streaming connection: ${clientId}; ${err}"`)
     }
-    return client
+    throw 'failed to get NATS streaming connection'
   }
 
   async attemptNack(conn: stan.Stan, msg: NATSMessage, timeout: number) {
@@ -89,7 +94,7 @@ class NatsUtil {
     try {
       conn.publish(this.config.natsDeadLetterSubject, msg.getRawData())
     } catch (err) {
-      console.log(`Error Nacking NATS message on subject: ${msg.getSubject}; ${err}"`);
+      console.log(`Error Nacking NATS message on subject: ${msg.getSubject}; ${err}"`)
     }
   }
   
@@ -98,4 +103,4 @@ class NatsUtil {
   }
 }
 
-export default NatsUtil;
+export default NatsUtil
