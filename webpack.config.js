6f3a8afd76adbf85924bd24b46746b7f3dfb66c1
Kyle Thomas <kyle@provide.services>
2020-03-05 22:30:53 -0500

    Use umd packaging

--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,7 @@ module.exports = {
     output: {
         path: path.resolve(__dirname),
         filename: 'dist/natsutil.js',
-        libraryTarget: 'commonjs',
+        libraryTarget: 'umd',
         library: 'natsutil',
     },
     resolve: {
