82e7ad8fd5fd9dacfd2ad63b620c9c68cd72da4b
Kyle Thomas <kyle@provide.services>
2020-03-23 01:07:13 -0400

    Cleanup webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -2,32 +2,31 @@ const path = require('path')
 const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
-    mode: 'production',
-    entry: path.resolve(__dirname, 'src/index.ts'),
-    externals: [
-        nodeExternals({
-          modulesDir: path.resolve(__dirname, './node_modules'),
-          whitelist: ['@provide/nats.ws'],
-        }),
-      ],
-    output: {
-        path: path.resolve(__dirname),
-	path: path.resolve(__dirname, 'dist', 'umd'),
-	filename: 'index.js',
-        libraryTarget: 'umd',
-        library: 'natsutil',
-        globalObject: 'this'
-    },
-    resolve: {
-        extensions: ['.js', '.ts'],
-    },
-    module: {
-        rules: [
-            {
-                test: /\.ts$/,
-                loader: "ts-loader",
-                exclude:  /(node_modules|test)/,
-            },
-        ],
-    }
+  mode: 'production',
+  entry: path.resolve(__dirname, 'src', 'index.ts'),
+  externals: [
+    nodeExternals({
+      modulesDir: path.resolve(__dirname, './node_modules'),
+      whitelist: ['@provide/nats.ws'],
+    }),
+  ],
+  output: {
+    path: path.resolve(__dirname, 'dist', 'umd'),
+    filename: 'index.js',
+    libraryTarget: 'umd',
+    library: 'natsutil',
+    globalObject: 'this'
+  },
+  resolve: {
+    extensions: ['.js', '.ts'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        loader: "ts-loader",
+        exclude:  /(node_modules|test)/,
+      },
+    ],
+  }
 };
